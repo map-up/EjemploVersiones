6bf7f1aaccc65dd7f187293832cc196b8cd417bf
map-up <141764918+map-up@users.noreply.github.com>
2023-08-08 16:36:33 -0300

    Update Clientes.java

--- src/Entidades/Clientes.java
+++ src/Entidades/Clientes.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package EjemploVersiones;
+package Entidades;
 
 /**
  *
  * @author Manuel
  */
 public class Clientes {
-    
+    //aqui iría algo pero no se me ocurre nada
 }
